f9ef6d688a9fc70992d3a61ee8093e92ca86901d
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-11-16 17:10:15 +0330

    Updating Spaceship operator file

--- Spaceship/main.cpp
+++ Spaceship/main.cpp
@@ -1,6 +1,6 @@
 // Ex2_07.cpp - The width, alignment, fill, and 0 formatting options of std::println()
-import <iostream>;
-import <compare>;
+#include <iostream>
+#include <compare>
 
 int main()
 {
@@ -9,7 +9,9 @@ int main()
     std::cin >> value;
     std::strong_ordering ordering{ value <=> 0};
 
-    std::cout <<"value < 0 :"<<ordering == std::storage_ordering::less << std::endl;
+    std::cout << "value < 0 :" << (ordering == std::strong_ordering::less) << std::endl;
+    std::cout << "value > 0 :" << (ordering == std::strong_ordering::greater) << std::endl;
+    std::cout << "value = 0 :" << (ordering == std::strong_ordering::equal) << std::endl;
 
 
 } 

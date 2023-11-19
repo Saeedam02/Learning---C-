8b76ec9429f900f6231e943dc631eab73a0a1b71
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-11-19 07:12:17 +0330

    Adding comparision main functions

---
+++ MinMax/comparison.cpp
@@ -0,0 +1,25 @@
+int max(int a, int b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+ 
+}
+
+int min(int a, int b)
+{
+    if (a < b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+ 
+}

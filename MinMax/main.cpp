7bf4b074fa02290ef9ed079d0494a92a380bf033
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-11-19 07:10:50 +0330

    Adding min and max functions in the form of include

--- MinMax/main.cpp
+++ MinMax/main.cpp
@@ -1,40 +1,13 @@
-
 #include <iostream>
 
-// Our Functions Declaration( prototype)
-int Max(int a, int b); 
-int Min(int a, int b);
+#include "comparision.h"
 
-int main()
-{
-    int x {5};
-    int y {10};
-  
-    std::cout << "max x and y is: " << Max(x,y) << std::endl;
-    std::cout << "min x and y is: " << Min(x,y) << std::endl;
-}
+int main() {
+  int x{5};
 
-int Max(int a, int b)
-{
-    if (a > b)
-    {
-        return a;
-    }
-    else
-    {
-        return b;
-    }
- 
-}
-int Min(int a, int b)
-{
-    if (a < b)
-    {
-        return a;
-    }
-    else
-    {
-        return b;
-    }
- 
+  int y{10};
+
+  std::cout << "max x and y is: " << max(x, y) << std::endl;
+
+  std::cout << "min x and y is: " << min(x, y) << std::endl;
 }

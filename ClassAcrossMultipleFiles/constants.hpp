1cbe79185c56c6a4f9eb75bb2e726f7b1f9e9f0a
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-11-20 09:48:13 +0330

    Adding Constants file

---
+++ ClassAcrossMultipleFiles/constants.hpp
@@ -0,0 +1,7 @@
+#ifndef CONSTANTS_H  // it's include gard which is gard against including sth
+                     // more than once.
+#define CONSTANTS_H
+
+const double PI{3.1415926535897932384626433832795};
+
+#endif

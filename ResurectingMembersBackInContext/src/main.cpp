08df6c6367720951c9dac4df9fa30fdfc4afb071
Saeed Aghamohammadi <saeed008190@gmail.com>
2023-11-26 14:16:51 +0330

    Adding main.cpp for resurecting members

---
+++ ResurectingMembersBackInContext/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "civilengineer.hpp"
+#include "engineer.hpp"
+#include "person.hpp"
+
+int main() {
+  Person person1;
+  Engineer en1;
+  CivilEngineer cic1;
+
+  std::cout << " all is done!" << std::endl;
+
+  return 0;
+}
